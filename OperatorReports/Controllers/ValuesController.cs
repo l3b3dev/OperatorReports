c47b8849a9b43d00404025c4079cf592557e5549
lebedev <yuriy.lebedev@drips.com>
2020-01-31 15:14:14 -0500

    api

--- OperatorReports/Controllers/ValuesController.cs
+++ OperatorReports/Controllers/ValuesController.cs
@@ -1,18 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using OperatorReports.Models;
 
 namespace OperatorReports.Controllers
 {
     public class ValuesController : ApiController
     {
         // GET api/values
-        public IEnumerable<string> Get()
+        public IEnumerable<OperatorReportViewModel> GetReports()
         {
-            return new string[] { "value1", "value2" };
+            var result = new List<OperatorReportViewModel>();
+
+            //TODO: roll in ORM like EF or Dapper for Db access
+            using (var conn =
+                    new SqlConnection(
+                        "Data Source=PHOENIX\\SQLEXPRESS;Initial Catalog=chat;User id=chat;Password=chat;") //TODO: move sensitive info like connection strings out
+            )
+            {
+                conn.Open();
+
+                using (var sqlcomm =
+                    new SqlCommand("exec dbo.OperatorProductivity ", conn))
+                {
+                    using (var dr = sqlcomm.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            var opVM = new OperatorReportViewModel
+                            {
+                                ID = Convert.ToInt32(dr[0]),
+                                Name = Convert.ToString(dr[1]),
+                                ProactiveSent = Convert.ToInt32(dr[2]),
+                                ProactiveAnswered = Convert.ToInt32(dr[3]),
+                                ProactiveResponseRate = Convert.ToInt32(dr[4]),
+                                ReactiveReceived = Convert.ToInt32(dr[5]),
+                                ReactiveAnswered = Convert.ToInt32(dr[6]),
+                                ReactiveResponseRate = Convert.ToInt32(dr[7]),
+                                TotalChatLength = Convert.ToString(dr[8]),
+                                AverageChatLength = Convert.ToString(dr[9])
+                            };
+                           
+                            result.Add(opVM);
+                        }
+                    }
+                }
+            }
+
+            return result;
         }
 
         // GET api/values/5
