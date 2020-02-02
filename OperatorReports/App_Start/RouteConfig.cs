8903c335a72b0eb878b0bab34746321f25f7b958
lebedev <yuriy.lebedev@drips.com>
2020-02-02 16:49:32 -0500

    removed styling controler and view

--- OperatorReports/App_Start/RouteConfig.cs
+++ OperatorReports/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace OperatorReports
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Styling", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
